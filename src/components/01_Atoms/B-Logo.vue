0c950555763cc22d245b5fdae1f56ef9125c2a17
medaka0228 <anywherefish@gmail.com>
2021-07-13 17:46:33 +0900

    Add SVG logo

---
+++ src/components/01_Atoms/B-Logo.vue
@@ -0,0 +1,19 @@
+<template>
+    <SVGElement />
+</template>
+
+<script>
+import SVGElement from "@/assets/VRWlogo_21-02-14_JP.svg"
+
+export default {
+    name: "VRWLogo",
+    props: {
+        lang: {
+            type: String,
+        },
+    },
+    components: {
+        SVGElement
+    },
+};
+</script>
--- src/components/04_Templates/HomeTemplate.vue
+++ src/components/04_Templates/HomeTemplate.vue
@@ -1,3 +1,15 @@
 <template>
-    <div class="p-mx-auto p-text-center">test</div>
+<div class="p-mx-auto p-text-center">
+    <VRWLogo />
+</div>
 </template>
+
+<script>
+import VRWLogo from "@/components/01_Atoms/B-Logo.vue"
+
+export default {
+    components: {
+        VRWLogo
+    }
+}
+</script>
