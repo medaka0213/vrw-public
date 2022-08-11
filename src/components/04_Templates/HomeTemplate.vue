a158fe0995eb21a46adb32ca5cd84c3621cef4e3
medaka0228 <anywherefish@gmail.com>
2022-08-11 19:17:25 +0900

    リンク追加

--- src/components/04_Templates/HomeTemplate.vue
+++ src/components/04_Templates/HomeTemplate.vue
@@ -21,6 +21,39 @@ DafaultLayout
             span.mr-3
                 a(href="/event")
                     | 集会一覧
+
+    .p-4.my-3.page-item.text-left
+        h3 外部リンク
+
+        p
+            |告知・スケジュール・打ち上げ情報の確認等にご利用ください
+        
+        span.pr-3  
+            a(href="https://twitter.com/vr_launch" target="_blank")
+                |Twitter
+        
+        span.pr-3
+            a(href="https://t.co/cjfnxjZrZG" target="_blank")
+                |Discord
+
+        span.pr-3
+            a(href="https://t.co/nL12021X0s" target="_blank")
+                |Google Calendar
+
+    .p-4.my-3.page-item.text-left
+        h3 ご連絡先
+        p
+            | ご意見・ご要望等ございましたら、下記のGoogle Form までお問い合わせください。
+
+        span.pr-3  
+            a(href="https://docs.google.com/forms/d/e/1FAIpQLScESvA557taE0zYj8gzlx2cQwrUDWs3cPxDnCWlacfUWTVLwg/viewform" target="_blank") 
+                |「ロケみる集会」参加者アンケート
+        
+        span.pr-3
+            a(href="https://docs.google.com/forms/d/e/1FAIpQLSeA5e7jdzyvLZD9-fs01ixzUJjJhcZKN65pjGSYj46nYYDEPw/viewform" target="_blank")
+                |「ロケみる集会」スタッフ参加希望フォーム
+
+
 </template>
 
 <script>
