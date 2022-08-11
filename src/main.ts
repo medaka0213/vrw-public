f6da21ac36db3e7ecedbe549fbccc862fe18a5b6
medaka0228 <anywherefish@gmail.com>
2022-08-11 18:19:27 +0900

    依存関係バトル

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import "bootstrap/dist/css/bootstrap.min.css"
 import 'primeflex/primeflex.css';
 import "primevue/resources/themes/saga-blue/theme.css"
 import "primevue/resources/primevue.min.css"
-import "primeicons/primeicons.css"
 
 import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
