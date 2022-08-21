f6740db39a0888ad3a1763a6ce406fce64673a51
medaka0228 <anywherefish@gmail.com>
2022-08-21 15:14:02 +0900

    console.log消した

--- src/models/mission.ts
+++ src/models/mission.ts
@@ -72,7 +72,6 @@ export class MissionBaseModel extends BaseModel implements MissionIF {
   }
 
   _extract_yt_id(url:string): string {
-    console.log(url)
     if (url.includes("youtube.com")){
       return url.split("v=")[1].split("&")[0]
     }else if (url.includes("youtu.be")){
